4ed1239440b21c111b1a32e5a0338af35b1a148a
Axel Leguía <axeleguia.chero@outlook.com>
2023-01-27 23:14:51 -0500

    Added timer alert end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public isAddTimerVisible: boolean = false;
+  public isEndTimerAlertVisible: boolean = false;
   public time: number = 0;
   public timers: Array<number> = [];
 
   constructor() {
     this.timers = [30, 15, 5];
-  }
-
-  logCountdownEnd() {
-    console.log('--countdown-end--');
   }
 
   public showAddTimer() {
@@ -30,6 +27,14 @@ export class AppComponent {
   public submitAddTimer() {
     this.timers.push(this.time);
     this.hideAddTimer();
+  }
+
+  public showEndTimerAlert() {
+    this.isEndTimerAlertVisible = true;
+  }
+
+  public hideEndTimerAlert() {
+    this.isEndTimerAlertVisible = false;
   }
 
 }
