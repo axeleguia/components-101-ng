7a2ee147503a2532e12b6a2cce7b9e763f806140
Axel Leguía <axeleguia.chero@outlook.com>
2023-01-27 23:33:42 -0500

    Added tabs with fake data

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Tab } from './tab.interface';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss']
 })
-export class TabComponent {
+export class TabComponent implements OnInit, Tab {
+
+  @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
+  @Input() title!: string;
+  public isActive: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  clickTabContent() {
+    this.onClick.emit();
+  }
 
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tab } from '../tab/tab.interface';
 
 @Component({
   selector: 'app-tabs',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent {
+
+  public tabs: Tab[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.addTab({ isActive: false, title: "tab 1" });
+    this.addTab({ isActive: false, title: "tab 2" });
+    this.addTab({ isActive: false, title: "tab 3" });
+    this.addTab({ isActive: false, title: "tab 4" });
+  }
+
+  addTab(tab: Tab) {
+    if (this.tabs.length === 0) {
+      tab.isActive = true;
+    }
+    this.tabs.push(tab);
+  }
+
+  selectTab(tab: Tab) {
+    for (let tab of this.tabs) {
+      tab.isActive = false;
+    }
+    tab.isActive = true;
+  }
 
 }
