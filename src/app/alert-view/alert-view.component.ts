d5b939eabc5df8a117991b67ceaf5b60dab70b76
Axel Leguía <axeleguia.chero@outlook.com>
2023-01-27 22:50:59 -0500

    Add alert-view

--- src/app/alert-view/alert-view.component.ts
+++ src/app/alert-view/alert-view.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alert-view',
   templateUrl: './alert-view.component.html',
   styleUrls: ['./alert-view.component.scss']
 })
-export class AlertViewComponent {
+export class AlertViewComponent implements OnInit {
+
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSubmit = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public isAddTimerVisible: boolean = false;
+  public time: number = 0;
+  
   constructor() { }
 
   logCountdownEnd() {
     console.log('--countdown-end--');
+  }
+
+  public showAddTimer() {
+    this.isAddTimerVisible = true;
+  }
+
+  public hideAddTimer() {
+    this.isAddTimerVisible = false;
   }
 
 }
